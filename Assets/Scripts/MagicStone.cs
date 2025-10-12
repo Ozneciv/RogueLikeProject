7f8d9ec6b93fe3d36699fc371dc8717aeed89e51
Ozneciv <vicenzodemarcoolivalves@gmail.com>
2025-10-12 01:55:44 -0300

    Bugfix

--- Assets/Scripts/MagicStone.cs
+++ Assets/Scripts/MagicStone.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class MagicStone_AI : MonoBehaviour
+public class MagicStone : MonoBehaviour
 {
     [Header("Referências")]
     public Transform playerTransform;
