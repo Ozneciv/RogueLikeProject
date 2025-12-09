8d81d0952cd4bc90b8f7502deb343e215c73ab03
Thiago_Travaglini <thiago.t.pires@hotmail.com>
2025-12-09 01:36:44 -0300

    Goblin_1

---
+++ Assets/Scripts/Goblin/GoblinBomb.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class GoblinBomb : MonoBehaviour
+{
+    public float raioExplosao = 4f;
+    public float dano = 20f;
+    //public GameObject efeitoExplosao; // Opcional
+    
+    // ... [Função OnCollisionEnter e Explodir aqui] ...
+}
